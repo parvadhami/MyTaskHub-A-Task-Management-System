416972341aff8877ac803399a6d899e1d267a2f5
Parva <95118321+parvadhami@users.noreply.github.com>
2024-03-16 11:36:57 +0530

    Uploading the homepage

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>TMS</title>
+    <!-- jQuery file -->
+    <script src="includes/jquery_latest.js"></script>
+    <!--Bootstrap-->
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- External CSS file -->
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-md-4 m-auto" id="home_page">
+            <center><h3> Choose Login</h3></center><br>
+            <a href="user_login.php" class="btn btn-success" style="margin-right: 20px">User Login</a>
+            <a href="register.php" class="btn btn-success" style="margin-right: 20px">User Registration</a>
+            <a href="admin/admin_login.php" class="btn btn-success">Admin Login</a>
+        </div>
+    </div>
+</body>
+</html>
